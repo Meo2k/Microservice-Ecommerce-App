693acfa1f958cb0afa6f866bef61e22f0d091e81
Meo2k <lovecatdat@gmail.com>
2026-01-23 22:29:18 +0700

    chore(be): update little code of main.ts in user service

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,14 +1,13 @@
 import express from 'express';
 import { ENV } from '@org/shared';
+import { userRouter } from './routes';
 
 const host = ENV.USER_SERVICE_HOST 
 const port = Number(ENV.USER_SERVICE_PORT)
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ 'message': 'Hello API'});
-});
+app.use('/', userRouter);
 
 app.listen(port, host, () => {
     console.log(`User Service is running on http://${host}:${port}`);
